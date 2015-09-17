4b6082bb6e23738c48e147b5b814bfbd7619f58c
Fay Koh <fay.koh@nyu.edu>
2015-09-17 16:19:03 -0400

    built ship, impoted environment assets

---
+++ hw03_treasurehunt/Assets/Scripts/BasicPlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicPlayerController : MonoBehaviour {
+
+	public float moveSpeed = 5f;
+	public float turnSpeed = 15f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{ 
+		if (Input.GetKey (KeyCode.A))
+		{
+			// move the current object, framerate independent
+			transform.position += new Vector3(0f, 0f, moveSpeed)* Time.deltaTime;
+			// rotate the current object to face the way it's moving
+			transform.eulerAngles = new Vector3(0f, turnSpeed, 0f) * Time.deltaTime;
+		}
+
+		Camera.main.transform.position = transform.position + new Vector3(0f, 5f, -25f);
+	}
+}
