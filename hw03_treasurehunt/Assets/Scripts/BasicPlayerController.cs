f5ff69a0544fe0a73ca41862a319d9415e7c3ba2
Fay Koh <fay.koh@nyu.edu>
2015-09-18 18:32:59 -0400

    created terrain and water

--- hw03_treasurehunt/Assets/Scripts/BasicPlayerController.cs
+++ hw03_treasurehunt/Assets/Scripts/BasicPlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class BasicPlayerController : MonoBehaviour {
 
 	public float moveSpeed = 5f;
-	public float turnSpeed = 15f;
+	public float turnSpeed = 90f;
 
 	// Use this for initialization
 	void Start () 
@@ -15,14 +15,43 @@ public class BasicPlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{ 
+
+		// move up
+		if (Input.GetKey (KeyCode.W))
+		{
+			// move the current object, framerate independent
+			transform.position += new Vector3(0f, 0f, 1f) * Time.deltaTime * moveSpeed;
+			// rotate the current object to face the way it's moving
+			transform.eulerAngles = new Vector3(0f, 1f, 0f);
+		}
+
+		// move left
 		if (Input.GetKey (KeyCode.A))
 		{
 			// move the current object, framerate independent
-			transform.position += new Vector3(0f, 0f, moveSpeed)* Time.deltaTime;
+			transform.position += new Vector3(-1f, 0f, 0f)* Time.deltaTime * moveSpeed;
 			// rotate the current object to face the way it's moving
-			transform.eulerAngles = new Vector3(0f, turnSpeed, 0f) * Time.deltaTime;
+			transform.eulerAngles = new Vector3(0f, -90f, 0f);
 		}
 
-		Camera.main.transform.position = transform.position + new Vector3(0f, 5f, -25f);
+		// move right
+		if (Input.GetKey (KeyCode.D))
+		{
+			// move the current object, framerate independent
+			transform.position += new Vector3(1f, 0f, 0f)* Time.deltaTime * moveSpeed;
+			// rotate the current object to face the way it's moving
+			transform.eulerAngles = new Vector3(0f, 90f, 0f);
+		}
+
+		// move down
+		if (Input.GetKey (KeyCode.S))
+		{
+			// move the current object, framerate independent
+			transform.position += new Vector3(0f, 0f, -1f)* Time.deltaTime * moveSpeed;
+			// rotate the current object to face the way it's moving
+			transform.eulerAngles = new Vector3(0f, 1f, 0f);
+		}
+		
+		Camera.main.transform.position = transform.position + new Vector3(0f, 2f, -8f);
 	}
 }
